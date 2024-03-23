5a03cc26f53de005faceed4d2ba77545e42cab52
ahmedali567 <ahmedalipathan134@gmail.com>
2024-03-23 20:37:10 +0500

    creating a cli based number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please select a number",
+},
+]);
+
+if (answers.userguessednumber === randomNumber) {
+    console.log("Congratulations you won.");
+}else{
+    console.log("you guessed wrong number");
+}
